236bdbd837c8b1fe1a16d7c7296ca188349e0855
ajouatom <ajouatom@gmail.com>
2022-07-07 17:28:29 +0900

    fix..

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -115,7 +115,7 @@ class CarInterface(CarInterfaceBase):
     ret.vEgoStarting = 0.5
     
     if candidate in (CAR.SANTA_FE, CAR.SANTA_FE_2022, CAR.SANTA_FE_HEV_2022, CAR.SANTA_FE_PHEV_2022):
-      ret.lateralTuning.pid.kf = 0.00005
+      #ret.lateralTuning.pid.kf = 0.00005
       ret.mass = 3982. * CV.LB_TO_KG + STD_CARGO_KG
       ret.wheelbase = 2.766
       # Values from optimizer
