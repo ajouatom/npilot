68861d62ae890f2f6c51e8ab6c96dcf49a826daf
ajouatom <ajouatom@gmail.com>
2022-07-11 18:58:59 +0900

    tt

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -380,7 +380,7 @@ void NvgWindow::drawStopLine(QPainter& painter, const UIState* s, const cereal::
     //painter.setBrush(QColor::fromRgbF(0.7, 0.0, 0.0, std::clamp<float>(stop_line_data.getProb(), 0.0, 0.7)));
     //painter.setBrush(QColor(255, 0, 0, 255));
     const vertex_data* v = &vd.v[0];
-    QPointF tt[] = { {v[0].x, v[0].y}, {v[1].x,[1].y}};
+    QPointF tt[] = { {v[0].x, v[0].y}, {v[1].x, v[1].y}};
     painter.setBrush(redColor(std::clamp<float>(stop_line_data.getProb()));
     painter.drawPolygon(tt, std::size(tt));
     //for (int i = 0; i < vd.cnt-1; i++) {
