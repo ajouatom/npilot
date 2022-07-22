5888887162a376165f3d4a256c247e27aaa6fe33
ajouatom <ajouatom@gmail.com>
2022-07-22 16:16:13 +0900

    stopline에서 정지하도록 해봄..

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -417,7 +417,8 @@ class LongitudinalMpc:
     # 정지준비가 되어 있을때, x에서 정지.. (stopline에서 정지가 맞지않나?, x나 stopline이나 비슷~)
     elif x[N] < 100.0 and self.on_stopping and not self.e2eMode:
       xstate = "STOPPING"
-      x_obstacles = np.column_stack([lead_0_obstacle, lead_1_obstacle, cruise_obstacle*1., x])
+      #x_obstacles = np.column_stack([lead_0_obstacle, lead_1_obstacle, cruise_obstacle*1., x])
+      x_obstacles = np.column_stack([lead_0_obstacle, lead_1_obstacle, cruise_obstacle*1., stopline])
     else:
       xstate = "CRUISE"
       self.on_stopping = False
