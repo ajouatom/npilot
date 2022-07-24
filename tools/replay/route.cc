b6b1e29e9a468141ad23189307d1eb7947c44975
ajouatom <ajouatom@gmail.com>
2022-07-24 12:59:24 +0900

    Revert "Revert "retro server...""
    This reverts commit c7913e10696f4c86ccecfc8121ab5e03684fd857.

--- tools/replay/route.cc
+++ tools/replay/route.cc
@@ -44,7 +44,7 @@ bool Route::loadFromServer() {
 
     loop.exit(success ? loadFromJson(json) : 0);
   });
-  http.sendRequest("https://api.commadotai.com/v1/route/" + route_.str + "/files");
+  http.sendRequest("hhttp://shind0.synology.me:3123/v1/route/" + route_.str + "/files");
   return loop.exec();
 }
 
